94ce901e2ba1fda6a559d48931b203b90d0ed2a1
juliluoyou <rainingliu0@gmail.com>
2018-03-07 22:56:35 +0800

    u

---
+++ src/main/java/com/example/myblogdev/MyblogdevApplication.java
@@ -0,0 +1,12 @@
+package com.example.myblogdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogdevApplication.class, args);
+	}
+}
